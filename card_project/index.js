5999746c810374e973f28f5844de2aa442128910
mariaatrinie <mariaatrinie@gmail.com>
2026-01-29 20:18:23 +0530

    fix render port, root route, mongodb connection

--- card_project/index.js
+++ card_project/index.js
@@ -15,9 +15,11 @@ app.get("/", (req, res) => {
   res.send("Hackathon backend is live 🚀");
 });
 
-
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 
 // POST
@@ -33,6 +35,9 @@ app.get("/cards", async (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
